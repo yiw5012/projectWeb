a0536755423c9f5e0734138b4a730c1ad38a4634
Metsuchart01 <suchartmoonsarn@gmail.com>
2025-03-01 01:47:39 +0700

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -7,8 +7,4 @@
 <a href="/NULLpage">NULLPAGE!!!!</a>
 <a href="/NULLpage">Yahooo!!!!</a>
 
-<<<<<<< HEAD
 <a href="/NULLpage">อยากลาออกกกกกกก</a>
-=======
-good coder??
->>>>>>> e122c0b4b78fc0fd2493565b9aaa1c4c9b1cc18c
