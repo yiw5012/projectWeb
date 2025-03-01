0e000c84e476de3a1dbecb1e7e3a8910a7b90d77
NineArmkun <66011212252@msu.ac.th>
2025-03-02 00:10:37 +0700

    ไม่มีไรอัพเดท เพิ่มบรรทัด

--- routes/createAct_post.php
+++ routes/createAct_post.php
@@ -6,4 +6,4 @@
 // $_POST["actname"];
 // $_POST["dateregisterend"];
 // $_POST["maxregister"];
-echo ($_POST["actname"] . $_POST["detailact"] . $_POST["location"] . $_POST["detailact"] . $_POST["dateevent"] . $_POST["actname"] . $_POST["dateregisterend"]. $_POST["maxregister"]);
+echo ($_POST["actname"] . $_POST["detailact"] . $_POST["location"] . $_POST["detailact"] . $_POST["dateevent"] . $_POST["actname"] . $_POST["dateregisterend"] . $_POST["maxregister"]);
