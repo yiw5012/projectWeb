bfb19b0a93eae3f4c6f1391cf51314b02c9a35d8
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-03-03 01:30:16 +0700

    Update home_get.php

--- routes/home_get.php
+++ routes/home_get.php
@@ -1,24 +1,18 @@
 <?php
 declare(strict_types=1);
 // ดึงข้อมูลผู้ใช้จากฐานข้อมูล
-if(isset($_SESSION['timestamp'])){
-    $result = getUserById($_SESSION['student_id']);
+$result = getUserById($_SESSION['student_id']);
+$events=getEvent();
+
+if (!isset($_GET['keyword'])) {
+    renderView('home_get', array('result' => $result,'events'=>$events));
+} elseif ($_GET['keyword'] == '') {
     $events=getEvent();
-    
-    if (!isset($_GET['keyword'])) {
-        renderView('home_get', array('result' => $result,'events'=>$events));
-    } elseif ($_GET['keyword'] == '') {
-        $events=getEvent();
-        renderView('home_get', array('result' => $result,'events'=>$events));
-    } else {
-        $events = getEventsByKeyword($_GET['keyword']);
-        renderView('home_get', array('result' => $result,'events'=>$events));
-    
-    }
+    renderView('home_get', array('result' => $result,'events'=>$events));
+} else {
+    $events = getEventsByKeyword($_GET['keyword']);
+    renderView('home_get', array('result' => $result,'events'=>$events));
+
 }
-renderView('home_get');
-
-
-
 // เรียกใช้งานฟังก์ชัน renderView เพื่อแสดงผลในหน้า home_get.php
 ?>
