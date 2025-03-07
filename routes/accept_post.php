8a33adf1475142ce39ec9d70574c6d5d1e11df9c
NineArmkun <66011212252@msu.ac.th>
2025-03-07 21:47:08 +0700

    update you can reject or accept user
     you can reject or accept user who request to your event if u r the once create event

--- routes/accept_post.php
+++ routes/accept_post.php
@@ -1,5 +1,8 @@
 <?php
-$_POST['case'];
-$_POST['event_id'];
-$_POST['user_id'];
+// $_POST['case'];
+// $_POST['event_id'];
+// $_POST['user_id'];
+
 reject_or_accept($_POST['case'], $_POST['event_id'], $_POST['user_id']);
+
+header('Location: /home');
