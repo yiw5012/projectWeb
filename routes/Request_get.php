6a1d895e5b40ad1567b9a49dfb0cd5fe35a89afc
NineArmkun <66011212252@msu.ac.th>
2025-03-23 01:52:30 +0700

    fix statistics function

--- routes/Request_get.php
+++ routes/Request_get.php
@@ -1,6 +1,6 @@
 <?php
 $result = get_registered_users_by_creator($_SESSION['student_id']);
 
-$statis = statistics_for_all(array('event' => $result));
+$statis = get_statistics($_SESSION['student_id']);
 print_r($statis);
 renderView("request_get",array('result' => $result, 'statistics' => $statis));
