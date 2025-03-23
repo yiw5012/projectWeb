f2f57ec80cba1a1591723156609d817a4d8f7246
NineArmkun <66011212252@msu.ac.th>
2025-03-23 22:52:59 +0700

    walkin_check

--- templates/check_get.php
+++ templates/check_get.php
@@ -2,10 +2,13 @@
 echo'fsfs'
 ?>
 <form action="check" method="post">
-
-              
+        <label for="registration_id">registration_id input</label>
+        <input type="text" id = 'registration_id'>        
+        <label for="otp">otp input</label>
         <input type="text" id = 'otp'>
+
          <button type="submit"></button>
+
 
 
 </form>
